42915224214c2e744b00dd0915ec6b49bc44f5fa
Manh <dtmanh1703@gmail.com>
2024-05-12 13:15:11 +0700

    i INIT

---
+++ test/test2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+    // int number[10]={0,0,0,0,0,0,0,0,0,0};
+    // for(int i=0;i<250;i++){
+    //     if (i>125){
+    //         number[i/25]+=1;
+    //     }
+    // }
+    // for (int i=0;i<10;i++){
+    //     printf("%d\n",number[i]);
+    // }
+    printf("%d",3%4);
+}
