1f4b63bb3ca72dc8738eea95aaed119a91c2a9dd
Manh <dtmanh1703@gmail.com>
2024-05-18 22:56:23 +0700

    INIT

--- main/network.c
+++
@@ -1,9 +0,0 @@
-#include "active_network.c"
-
-int main() {
-    struct core cores[5];
-    get_parameter(cores);
-    active_network(cores);
-    get_network_accuracy();
-    return 0;
-}
---
+++ neuron_network/active_network.c
@@ -0,0 +1,126 @@
+#include "get_parameter.c"
+#include "get_packet.c"
+#include "get_network_accuracy.c"
+
+#define class 10
+
+void neuron_block_update_potential(struct core cores[], int *des_core, int *des_axon){
+    for(int neuron = 0; neuron < cores[*des_core].max_neurons; neuron++){
+        if(cores[*des_core].neurons[neuron].conns[*des_axon]){
+            cores[*des_core].neurons[neuron].current_potential += cores[*des_core].neurons[neuron].formatted_weight[cores[*des_core].axons[*des_axon].axon_type];
+        }
+    }
+}
+
+void check_threshold_and_fire(struct core cores[]){
+    int des_core_packet=0;
+    int des_axon_packet=0;
+    int dx=0;
+    int dy=0;
+    for(int core=0;core<(max_core-1);core++){
+        for(int neuron=0; neuron < cores[core].max_neurons;neuron++){
+
+            if(cores[core].neurons[neuron].current_potential >= cores[core].neurons[neuron].pos_threshold){
+                cores[core].neurons[neuron].current_potential = cores[core].neurons[neuron].reset_potential;
+                dx = core%4 + cores[core].neurons[neuron].spike_des_core[0];
+                dy = core/4 + cores[core].neurons[neuron].spike_des_core[1];
+                des_core_packet = dx + 4*dy;
+
+                des_axon_packet = cores[core].neurons[neuron].spike_des_axon;
+
+                neuron_block_update_potential(cores,&des_core_packet,&des_axon_packet);
+
+            }
+            if(cores[core].neurons[neuron].current_potential < cores[core].neurons[neuron].neg_threshold){
+                cores[core].neurons[neuron].current_potential = cores[core].neurons[neuron].reset_potential;
+            }
+        }
+    }
+}
+
+void get_vote_core_5(struct core cores[],int number []){
+    int i=0;
+    for(int neuron=0;neuron<cores[max_core-1].max_neurons;neuron++){
+
+        if(cores[max_core-1].neurons[neuron].current_potential >= cores[max_core-1].neurons[neuron].pos_threshold){
+            number[i] += 1;
+            cores[max_core-1].neurons[neuron].current_potential = cores[max_core-1].neurons[neuron].reset_potential;
+
+        }
+        if(cores[max_core-1].neurons[neuron].current_potential < cores[max_core-1].neurons[neuron].neg_threshold){
+            cores[max_core-1].neurons[neuron].current_potential = cores[max_core-1].neurons[neuron].reset_potential;
+        }
+        if(i==(class-1)){
+            i=0;
+        }
+        else{
+            i=i+1;
+        }
+    }
+}
+void save_network_vote_class(FILE *f_save_network_predict,int number[],int mnist,int predict){
+    fprintf(f_save_network_predict,"Vote for image    %d\n",mnist+1);
+    for(int num=0;num<class;num++){
+        fprintf(f_save_network_predict,"Number %d: %d\n",num,number[num]);
+    }
+    fprintf(f_save_network_predict,"Network predict: number %d\n",predict);
+    fprintf(f_save_network_predict,"------------------------\n");
+}
+void get_predict(FILE *f_predict,FILE *f_save_network_predict,int number[],int mnist){
+    int get_max=0;
+    int predict=0;
+    for(int i=(class-1);i>=0;i--){
+        if(get_max <= number[i]){
+            predict=i;
+            get_max=number[i];
+        }
+    }
+    fprintf(f_predict,"%d\n",predict);
+    save_network_vote_class(f_save_network_predict,number,mnist,predict);
+}
+
+void active_network(struct core cores[]){
+    int des_core = 0 ;
+    int des_axon = 0;
+    int num_inputs = 0;
+    FILE *f_num_inputs=fopen("./testbench/tb_num_inputs.txt","r");
+    if (f_num_inputs == NULL) {
+        printf("Error opening file tb_num_inputs.txt\n");
+        exit(1);
+    }
+    FILE *f_input=fopen("./testbench/tb_input.txt","r");
+    if (f_input == NULL) {
+        printf("Error opening file tb_inputs.txt\n");
+        exit(1);
+    }
+
+    FILE *f_save_packet=fopen("./packet/packet_mnist.txt","w");
+    FILE *f_predict=fopen("./predict/predict.txt","w");
+    FILE *f_save_network_predict = fopen("./predict/vote_class.txt","w");
+
+    for(int mnist=0;mnist<max_mnist;mnist++){
+        int number[class];
+        for(int c=0;c<class;c++){
+            number[c]=0;
+        }
+        fprintf(f_save_packet,"MNIST %d: num_inputs: %d\n",mnist+1,num_inputs=get_num_inputs(f_num_inputs));
+
+        for(int packet=0;packet<num_inputs;packet++){
+            get_des_packet(f_input,&des_core,&des_axon);
+            fprintf(f_save_packet,"packet %d: des_core: %d ",packet+1,des_core);
+            fprintf(f_save_packet," des_axon: %d\n",des_axon);
+            neuron_block_update_potential(cores,&des_core,&des_axon);
+        }
+
+        check_threshold_and_fire(cores);
+
+        get_vote_core_5(cores,number);
+        get_predict(f_predict,f_save_network_predict,number,mnist);
+
+    }
+    fclose(f_input);
+    fclose(f_num_inputs);
+    fclose(f_save_packet);
+    fclose(f_predict);
+    fclose(f_save_network_predict);
+}        
