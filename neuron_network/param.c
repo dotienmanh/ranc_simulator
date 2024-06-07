211b79e5cb6ce0551353474070b618dd921ff76e
Manh <dtmanh1703@gmail.com>
2024-06-07 10:46:17 +0700

    INIT

--- neuron_network/param.c
+++ neuron_network/param.c
@@ -1,7 +1,7 @@
 #define max_mnist  10000
 #define class  10
 #define max_packet max_axon*4
-#define PATH "D:/Code/parameter/param78"
+#define PATH "D:/Code/c_code/parameter/parameter4"
 
 /*** Tham số cho network ***/
 #define num_layer 4
